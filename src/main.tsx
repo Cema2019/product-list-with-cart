6bf885f5954f032bf841bfc0701a6a53a36639d1
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-11-26 21:19:31 -0600

    Update import name for Cart.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./components/Card.css";
-import "./components/Carrito.css";
+import "./components/Cart.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ParentComponent from "./components/ParentComponent";
 import "./components/Order.css";
