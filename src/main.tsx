65b220247c0bf7377c686cda3e54bcce192e9c86
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-12-23 15:44:21 -0600

    style(css): remove card.css file and remove bootstrap import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./components/Cart.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import ParentComponent from "./components/ParentComponent";
 
 
